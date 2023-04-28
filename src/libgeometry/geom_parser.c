d72ace13888de40d9adad61a8f878ef5832fff2c
Oikapi <seryy.gordeev.78@mail.ru>
2023-04-28 19:01:40 +0700

    fix-beta-lab4

--- src/libgeometry/geom_parser.c
+++ src/libgeometry/geom_parser.c
@@ -1,11 +1,12 @@
+#include "geom_parser.h"
+#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include "geom_parser.h"
 
 int check_real_lenght(struct Figure main_check_circle) {
     int k = 0;
-    for (int i = 0; i < (int)sizeof(main_check_circle.thirdx) / (int)sizeof(main_check_circle.thirdx[0]); i++) {
+    for (int i = 0; i < (int) sizeof(main_check_circle.thirdx) / (int) sizeof(main_check_circle.thirdx[0]); i++) {
         if ((int) main_check_circle.thirdx[i] != 32) {
             k++;
         } else {
@@ -15,21 +16,19 @@ int check_real_lenght(struct Figure main_check_circle) {
     return k;
 }
 
- void circle_print(struct Figure fig){
-    // printf("circle(");
-    // printf("%f")
+void circle_print(struct Figure fig) {
     printf("%s ", fig.figure_firstx);
     printf("%s ", fig.secondx);
-    printf("%s" , fig.thirdx);
- }
+    printf("%s", fig.thirdx);
+}
 
-int check_x(char string[], int n,int *main_error) {
-    int  dot = '.';
+int check_x(char string[], int n, int *main_error) {
+    int dot = '.';
     int minus = '-';
     int dot_flag = 0;
-    int minus_flag = 0 ;
+    int minus_flag = 0;
     for (int i = 0; i < n; i++) {
-        if ((int) string[i] == dot ) {
+        if ((int) string[i] == dot) {
             dot_flag += 1;
         }
         if ((int) string[i] == minus) {
@@ -37,21 +36,21 @@ int check_x(char string[], int n,int *main_error) {
         }
         if (dot_flag > 1) {
             *main_error = 9;
-            return i+1;
+            return i + 1;
         }
-        if(minus_flag > 1){
+        if (minus_flag > 1) {
             *main_error = 3;
-            return i+1;
+            return i + 1;
         }
-        if (((int) string[i] > (int)('9') || (int) string[i] < (int)('0')) && ((int) string[i] != minus && (int) string[i] != dot )) {
+        if (((int) string[i] > (int) ('9') || (int) string[i] < (int) ('0')) && ((int) string[i] != minus && (int) string[i] != dot)) {
             *main_error = 3;
-            return i+1;
+            return i + 1;
         }
     }
     return 0;
 }
 
-int check_radius(char string[], int n,int *main_error){
+int check_radius(char string[], int n, int *main_error) {
     int dot = '.';
     int minus = '-';
     int flag = 0;
@@ -59,15 +58,15 @@ int check_radius(char string[], int n,int *main_error){
         if ((int) string[i] == dot) {
             flag += 1;
         }
-        if((int) string[i] == minus){
+        if ((int) string[i] == minus) {
             *main_error = 10;
-            return i+1;
+            return i + 1;
         }
         if (flag > 1) {
             *main_error = 9;
             return i + 1;
         }
-        if (((int) string[i] > (int)('9') || (int) string[i] < (int)('0')) && string[i] != (int)('.') ) {
+        if (((int) string[i] > (int) ('9') || (int) string[i] < (int) ('0')) && string[i] != (int) ('.')) {
             *main_error = 3;
             return i + 1;
         }
@@ -76,7 +75,7 @@ int check_radius(char string[], int n,int *main_error){
 }
 
 
-int check_loss(struct Figure main_check_circle, char str[],int*main_error) {
+int check_loss(struct Figure main_check_circle, char str[], int *main_error) {
     if (strlen(main_check_circle.thirdx) - strlen(str) > 2) {
         *main_error = 8;
         return 1;
@@ -84,7 +83,7 @@ int check_loss(struct Figure main_check_circle, char str[],int*main_error) {
     return 0;
 }
 
-int check_comma(struct Figure main_check_circle,int *main_error) {
+int check_comma(struct Figure main_check_circle, int *main_error) {
     if (main_check_circle.secondx[strlen(main_check_circle.secondx) - 1] != 44) {
         *main_error = 5;
         return 3;
@@ -92,8 +91,8 @@ int check_comma(struct Figure main_check_circle,int *main_error) {
     return 0;
 }
 
-int check_last_bracket(struct Figure main_check_circle,int *main_error) {
-    for (int i = 0; i < (int)strlen(main_check_circle.thirdx); i++) {
+int check_last_bracket(struct Figure main_check_circle, int *main_error) {
+    for (int i = 0; i < (int) strlen(main_check_circle.thirdx); i++) {
         if (main_check_circle.thirdx[i] == 41) {
             return 0;
         }
@@ -103,10 +102,10 @@ int check_last_bracket(struct Figure main_check_circle,int *main_error) {
     return 3;
 }
 
-int check_name(struct Figure main_check_circle,int *main_error) {
+int check_name(struct Figure main_check_circle, int *main_error) {
     char check_name[] = "circle(";
-    
-    for (int i = 1; i <= (int)strlen(check_name); i++) {
+
+    for (int i = 1; i <= (int) strlen(check_name); i++) {
         if (strncmp(check_name, main_check_circle.figure_firstx, i) != 0) {
             if (i == 7) {
                 *main_error = 4;
@@ -119,7 +118,7 @@ int check_name(struct Figure main_check_circle,int *main_error) {
     }
     return 0;
 }
-void print_pointer(struct Figure fig, int pointer){
+void print_pointer(struct Figure fig, int pointer) {
     printf("%s ", fig.figure_firstx);
     printf("%s ", fig.secondx);
     printf("%s ", fig.thirdx);
@@ -145,7 +144,7 @@ void print_error(int main_error) {
     printf("\n");
 }
 
-void load_x(struct Int_figure *fig,struct Figure main_check_circle){
+void load_x(struct Int_figure *fig, struct Figure main_check_circle) {
     char x1[20] = "";
     char x2[20] = "";
     char x3[20] = "";
@@ -155,33 +154,32 @@ void load_x(struct Int_figure *fig,struct Figure main_check_circle){
     strcpy(x1, main_check_circle.figure_firstx + strlen(main_check_circle.figure_firstx) - length_figure_firstx + length_name);
     strncpy(x2, main_check_circle.secondx, strlen(main_check_circle.secondx) - 1);
     strncpy(x3, main_check_circle.thirdx, strlen(main_check_circle.thirdx) - 1);
-    
-    fig -> x1 = atof(x1);
-    fig -> x2 = atof(x2);
-    fig -> radius = atof(x3);
+
+    fig->x1 = atof(x1);
+    fig->x2 = atof(x2);
+    fig->radius = atof(x3);
 }
 
-int isIntersection(struct Int_figure fig, struct Int_figure fig2){
-    double d = sqrt(pow(fig.x1 - fig2.x1 ,2) + pow(fig.x2 - fig2.x2 ,2) );
-        if(pow(fig2.radius - fig.radius,2) > pow(d,2)){
-            printf("One circle in other\n");
-            return 2;
-        }
-        else if(d < (fig.radius + fig2.radius)){
-            printf("There is a intersetion\n");
-            return 1;
-        } 
-        if(d > (fig.radius + fig2.radius)){
-            printf("There is no intersetion\n");
-            return 0;
-        }
-
+int isIntersection(struct Int_figure fig, struct Int_figure fig2) {
+    double d = sqrt(pow(fig.x1 - fig2.x1, 2) + pow(fig.x2 - fig2.x2, 2));
+    if (pow(fig2.radius - fig.radius, 2) > pow(d, 2)) {
+        printf("One circle in other\n");
+        return 2;
+    } else if (d < (fig.radius + fig2.radius)) {
+        printf("There is a intersetion\n");
+        return 1;
+    }
+    if (d > (fig.radius + fig2.radius)) {
+        printf("There is no intersetion\n");
+        return 0;
+    }
+    return 3;
 }
 
-int mainCheck(struct Figure main_check_circle,int *pointer) {
+int mainCheck(struct Figure main_check_circle, int *pointer) {
     int main_error;
-    if (check_name(main_check_circle,&main_error) != 0) {
-        *pointer = check_name(main_check_circle,&main_error)-1 ;
+    if (check_name(main_check_circle, &main_error) != 0) {
+        *pointer = check_name(main_check_circle, &main_error) - 1;
         return main_error;
     }
     int length_figure_firstx = strlen(main_check_circle.figure_firstx);
@@ -193,31 +191,29 @@ int mainCheck(struct Figure main_check_circle,int *pointer) {
     strcpy(x1, main_check_circle.figure_firstx + strlen(main_check_circle.figure_firstx) - length_figure_firstx + length_name);
     strncpy(x2, main_check_circle.secondx, strlen(main_check_circle.secondx) - 1);
     strncpy(x3, main_check_circle.thirdx, check_real_lenght(main_check_circle) - 1);
-    *pointer = check_x(x1, strlen(x1),&main_error) + strlen(check_name) +1;
-    printf("%s %d %s\n" , x1, strlen(x1) ,x3);
-    if (check_x(x1, strlen(x1),&main_error) != 0) {
+    *pointer = check_x(x1, strlen(x1), &main_error) + strlen(check_name) + 1;
+    if (check_x(x1, strlen(x1), &main_error) != 0) {
         return main_error;
     }
-    *pointer += check_x(x2, strlen(main_check_circle.secondx),&main_error) + strlen(x1)+1;
-    if (check_x(x2, strlen(x2),&main_error) != 0) {
+    *pointer += check_x(x2, strlen(main_check_circle.secondx), &main_error) + strlen(x1) + 1;
+    if (check_x(x2, strlen(x2), &main_error) != 0) {
         return main_error;
     }
-    *pointer += strlen(main_check_circle.secondx) ;
-    if (check_comma(main_check_circle,&main_error) != 0) {
+    *pointer += strlen(main_check_circle.secondx);
+    if (check_comma(main_check_circle, &main_error) != 0) {
         return main_error;
     }
-    *pointer += (check_x(x3, strlen(x3),&main_error)) + 1;
-    if (check_radius(x3, strlen(x3)-1,&main_error) != 0) {
+    *pointer += (check_x(x3, strlen(x3), &main_error)) + 1;
+    if (check_radius(x3, strlen(x3) - 1, &main_error) != 0) {
         return main_error;
     }
     *pointer += strlen(x3) - 1;
-    if (check_last_bracket(main_check_circle,&main_error) != 0) {
+    if (check_last_bracket(main_check_circle, &main_error) != 0) {
         return main_error;
     }
     *pointer += strlen(main_check_circle.thirdx) - strlen(x3) - 1;
-    if (check_loss(main_check_circle, x3,&main_error) != 0) {
+    if (check_loss(main_check_circle, x3, &main_error) != 0) {
         return main_error;
     }
     return 0;
 }
-
--- test/circle_test.c
+++ test/circle_test.c
@@ -7,8 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-CTEST(GEOM_PARSER, isIntersection)
-{
+CTEST(FIND_INTERSECTION, isIntersection) {
     struct Int_figure One_in_other[2];
     One_in_other[0].x1 = 10;
     One_in_other[0].x2 = 10;
@@ -16,15 +15,15 @@ CTEST(GEOM_PARSER, isIntersection)
     One_in_other[1].x1 = 10;
     One_in_other[1].x2 = 10;
     One_in_other[1].radius = 2;
-    
+
     struct Int_figure Intersec_circles[2];
     Intersec_circles[0].x1 = 1;
     Intersec_circles[0].x2 = 2;
     Intersec_circles[0].radius = 3;
     Intersec_circles[1].x1 = 2;
     Intersec_circles[1].x2 = 2;
-    Intersec_circles[1].radius = 3 ;
-    
+    Intersec_circles[1].radius = 3;
+
     struct Int_figure No_intersec_circles[2];
     No_intersec_circles[0].x1 = 1;
     No_intersec_circles[0].x2 = 50;
@@ -34,37 +33,36 @@ CTEST(GEOM_PARSER, isIntersection)
     No_intersec_circles[1].radius = 3;
 
     int a = isIntersection(One_in_other[0], One_in_other[1]);
-    ASSERT_EQUAL(a,2);
+    ASSERT_EQUAL(a, 2);
 
     int b = isIntersection(Intersec_circles[0], Intersec_circles[1]);
-    ASSERT_EQUAL(b,1);
+    ASSERT_EQUAL(b, 1);
 
     int c = isIntersection(No_intersec_circles[0], No_intersec_circles[1]);
-    ASSERT_EQUAL(c,0);
+    ASSERT_EQUAL(c, 0);
 }
 
-CTEST(GEOM_PARSER, mainCheck)
-{
+CTEST(FIND_INVALID_INPUT, mainCheck) {
     int pointer;
     struct Figure circle[4];
-    char exampls[4][25]= {"circle(10.4 10, 10)","crcle(10.3 10.6, 10)","circle(10 10, 10","circle(10 aa, 10)"};
-    for(int i = 0 ; i < 4;i++){
+    char exampls[4][25] = {"circle(10.4 10, 10)", "crcle(10.3 10.6, 10)", "circle(10 10, 10", "circle(10 aa, 10)"};
+    for (int i = 0; i < 4; i++) {
         char *token = strtok(exampls[i], " ");
         strcpy(circle[i].figure_firstx, token);
         strcpy(circle[i].secondx, strtok(NULL, " "));
         strcpy(circle[i].thirdx, strtok(NULL, " "));
     }
 
-    int a = mainCheck(circle[0],&pointer);
+    int a = mainCheck(circle[0], &pointer);
     ASSERT_EQUAL(a, 0);
 
-    a = mainCheck(circle[1],&pointer);
+    a = mainCheck(circle[1], &pointer);
     ASSERT_EQUAL(a, 2);
 
-    a = mainCheck(circle[2],&pointer);
+    a = mainCheck(circle[2], &pointer);
     ASSERT_EQUAL(a, 7);
 
-    a = mainCheck(circle[3],&pointer);
+    a = mainCheck(circle[3], &pointer);
     ASSERT_EQUAL(a, 3);
 }
 
